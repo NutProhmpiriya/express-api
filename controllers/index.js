4b4489f9fddd3cab2a924795ea8a9dc0e4144415
Prohmpiriya Phonumnuaisuk <prohmpiriya@MacbookPro14A.local>
2023-02-13 14:36:04 +0700

    fix syntax

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,8 @@
 
-import loggerMiddleware from '../middleware/logger.js'
 import productsController from './product.controllers.js'
 
 export default async function intiRouter(app) {
     try {
-    app.use(loggerMiddleware)
-
     app.use('/products', productsController)
 
     } catch (error) {
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import intiRouter from './controllers/index.js';
+import middlewares from './middleware/index.js';
+
 const app = express();
 const port = 5001;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(middlewares.logger)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
